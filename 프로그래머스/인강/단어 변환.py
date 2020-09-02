480016f3de6e8f267354aaea06dbfdb99f1153a2
somi <cindy960602@naver.com>
2020-09-02 20:20:12 +0900

    algorithm

--- 프로그래머스/인강/단어 변환.py
+++ 프로그래머스/인강/단어 변환.py
@@ -11,14 +11,17 @@ def solution(begin, target, words):
                 # 이미 비교한 단어는 비교하지 않음
                 if visit[i]==1:
                     continue
-
+                
+                #begin단어랑 비교될단어랑 글자차이수=cnt
                 cnt=0
                 for j in range(len(word)):
                     if word[j]!=words[i][j]:
                         cnt+=1
-
+                
+                #글자가 하나만 다르면 dfs탐색
                 if cnt==1:
                     visit[i]=1
+                    print(words[i],depth+1)
                     dfs(words[i],depth+1)
                     visit[i]=0
     dfs(begin,0)
